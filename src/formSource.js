757717afa35f92ad5b034c32c8ff213d6c558229
amritamazumder <106809438+amritamazumder@users.noreply.github.com>
2022-06-28 13:13:17 +0530

    Added form source

---
+++ src/formSource.js
@@ -0,0 +1,151 @@
+export const userInputs=[
+    {
+        id: 1,
+        label: "User Name",
+        type: "text",
+        placeholder: "john_doe"
+
+    },
+    {
+        id: 2,
+        label: "First Name",
+        type: "text",
+        placeholder: "John"
+    },
+    {
+        id: 3,
+        label: "Last Name",
+        type: "text",
+        placeholder: "Doe"
+    },
+    {
+        id: 4,
+        label: "Email",
+        type: "text",
+        placeholder: "j.doe@exampleclinic.com"
+    },
+    {
+        id: 5,
+        label: "Password",
+        type: "password"
+    },
+    {
+        id: 6,
+        label: "Phone",
+        type: "text",
+        placeholder: "+91234567890"
+    },
+    {
+        id: 7,
+        label: "Position",
+        type: "text",
+        placeholder: "registered nurse"
+    },
+    {
+        id: 8,
+        label: "Department",
+        type: "text",
+        placeholder: "oncology"
+    },
+    {
+        id: 9,
+        label: "Employment Type",
+        type: "text",
+        placeholder: "contract"
+    }
+
+]
+
+export const patientInputs=[
+    {
+        id: 1,
+        label: "First Name",
+        type: "text",
+        placeholder: "John"
+    },
+    {
+        id: 2,
+        label: "Last Name",
+        type: "text",
+        placeholder: "Smith"
+    },
+    {
+        id: 3,
+        label: "Email",
+        type: "text",
+        placeholder: "jsmith@gmail.com"
+    },
+    {
+        id: 4,
+        label: "Phone",
+        type: "text",
+        placeholder: "+91234567890"
+    },
+    {
+        id: 5,
+        label: "First Name of Next of Kin",
+        type: "text",
+        placeholder: "Johanna"
+    },
+    {
+        id: 6,
+        label: "Last Name of Next of Kin",
+        type: "text",
+        placeholder: "Johanna"
+    },
+    {
+        id: 7,
+        label: "Email of Next of Kin",
+        type: "text",
+        placeholder: "johannasmith@gmail.com"
+    },
+    {
+        id: 8,
+        label: "Phone of Next of Kin",
+        type: "text",
+        placeholder: "+9123456789"
+    },
+    {
+        id: 9,
+        label: "Patient Age",
+        type: "text",
+        placeholder: "60"
+    },
+    {
+        id: 10,
+        label: "Complaint",
+        type: "text",
+        placeholder: "knee pain"
+    },
+    {
+        id: 11,
+        label: "Department Admitted",
+        type: "text",
+        placeholder: "rheumatology"
+    },
+    {
+        id: 12,
+        label: "Doctor Assigned",
+        type: "text",
+        placeholder: "Dr Shaun Murphy"
+    },
+    {
+        id: 13,
+        label: "Nurse Assigned",
+        type: "text",
+        placeholder: "Nurse Maggie"
+    },
+    {
+        id: 14,
+        label: "Drugs Patient is On",
+        type: "text",
+        placeholder: "aspirin.."
+    },
+    {
+        id: 15,
+        label: "Additional Comments",
+        type: "text",
+        placeholder: "Has a history of heart disease"
+    },
+
+]
